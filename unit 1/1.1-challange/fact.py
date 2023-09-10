31c41b4d6a74136e6faf359e6091ccb396a3565b
mahendran143 <144576248+mahendran143@users.noreply.github.com>
2023-09-09 23:57:31 -0700

    Add files via upload

---
+++ unit 1/1.1-challange/fact.py
@@ -0,0 +1,14 @@
+"""1.1-Implement a recursive function to calculte the 
+factorial of a given number"""
+
+def fact(n):
+	if n==0:
+		return 1
+	else:
+		return n*fact(n-1)
+n=int(input("Enter the number:"))
+result=fact(n)
+print("The factorial of {} is {} :".format(n,result))		
+
+
+		
